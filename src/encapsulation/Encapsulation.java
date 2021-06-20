c9704dc15dbed6baa386c4c037102488655cfa4c
ShashiniU <uwanshika1997@gmail.com>
2021-06-20 17:33:51 +0530

    first commit

---
+++ src/encapsulation/Encapsulation.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package encapsulation;
+
+/**
+ *
+ * @author A C E R
+ */
+public class Encapsulation {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Uwanshika uwanshika = new Uwanshika();
+        
+        
+        uwanshika.sethealth(5);
+        uwanshika.gethealth();
+        // TODO code application logic here
+    }
+    
+}
---
+++ src/encapsulation/Enemy.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package encapsulation;
+
+/**
+ *
+ * @author A C E R
+ */
+public class Enemy {
+     private int health;
+     
+     public void gethealth(){
+         System.out.println(health); 
+     }
+     public void sethealth(int x){
+         health = x;
+     }
+    
+    
+}
